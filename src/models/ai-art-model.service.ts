3f2eadc438d61b6659fbab7a6339aaaa3e9f9fc9
karafra <darius.kralovic1@protonmail.com>
2022-06-10 16:41:57 +0200

    :tada: Initial commit

---
+++ src/models/ai-art-model.service.ts
@@ -0,0 +1,25 @@
+import { AiArtResponse } from '../types/api/ai-art';
+import { HttpClient } from '@Interceptors/http-client';
+import { AxiosResponse } from 'axios';
+
+class AiArtModelService extends HttpClient {
+  public constructor() {
+    super('https://bf.dallemini.ai');
+  }
+
+  private getResponse(prompt: string): Promise<AxiosResponse<AiArtResponse>> {
+    return this.instance.post('/generate', {
+      prompt,
+      headers: {
+        Accept: 'application/json'
+      }
+    });
+  }
+
+  public async getImageArray(prompt: string): Promise<AiArtResponse> {
+    const { data } = await this.getResponse(prompt);
+    return data;
+  }
+}
+
+export const aiArtModelService = new AiArtModelService();
