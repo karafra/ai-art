9f3b1d516293c203de2e0ab361a7db7f1d7eb527
karafra <darius.kralovic1@protonmail.com>
2022-06-13 01:53:54 +0200

    :necktie: Upadate timeouts and retries

--- src/services/ai-art.service.ts
+++ src/services/ai-art.service.ts
@@ -4,13 +4,13 @@ import { Collage } from '@Utils/collage';
 export class AiArtService {
   public async getArt(prompt: string): Promise<Buffer> {
     const collage = new Collage();
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < 30; i++) {
       try {
         const response = await aiArtModelService.getImageArray(prompt);
         await collage.constructCollage(response.images);
         return collage.canvas.toBuffer('image/png');
       } catch (Error) {
-        await new Promise((f) => setTimeout(f, 2000));
+        await new Promise((f) => setTimeout(f, 3000));
         continue;
       }
     }
